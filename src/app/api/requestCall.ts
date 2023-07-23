2ede5daaa391d3f749535ea3b57a2f5d24662bba
Nicholas Koben Kao <kobenkao@gmail.com>
2023-07-22 21:14:44 -0700

    env in request call and remove old route.ts

--- src/app/api/requestCall.ts
+++ src/app/api/requestCall.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
+import dotenv from "dotenv";
+dotenv.config({ path: `.env.local` });
 
 interface RequestBody {
   phoneNumber: string;
