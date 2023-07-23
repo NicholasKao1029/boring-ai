94d37b322144087733e75e688d57b6b82585f8bc
Nicholas Koben Kao <kobenkao@gmail.com>
2023-07-22 17:31:57 -0700

    blah'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,47 @@
-import Image from 'next/image'
+'use client'
 import {Button} from '@/components/ui/button'
 import {Input} from '@/components/ui/input'
-import {TextArea} from '@/components/ui/TextArea'
+import {Textarea} from '@/components/ui/textarea'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [objective, setObjective] = useState('')
+  const [transcript, setTranscript] = useState('')
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col space-y-4 lg:w-1/2 p-4">
-          <Input placeholder="Phone Number" />
-          <TextArea placeholder="Objective" />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl font-mono text-sm lg:flex flex-col space-y-4">
+          <div className="flex lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+            <div className="flex flex-col space-y-4 lg:w-1/2">
+              <Input 
+                placeholder="Phone Number" 
+                value={phoneNumber} 
+                onChange={(e) => setPhoneNumber(e.target.value)} 
+              />
+              <Textarea 
+                placeholder="Objective" 
+                value={objective} 
+                onChange={(e) => setObjective(e.target.value)} 
+              />
+            </div>
+            <div className="flex lg:w-1/2 justify-end items-start">
+              <Button>Give to user</Button>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-1 lg:w-1/2">
+            <h3 className="font-bold text-lg">Transcript</h3>
+            <Textarea 
+              placeholder="Transcript" 
+              readOnly 
+              value={transcript}
+            />
+          </div>
         </div>
-        <div className="flex lg:w-1/2 p-4 justify-end items-start">
-          <Button>Add to List</Button>
-        </div>
-      </div>
-
-      {/* Rest of your code here */}
-
-    </main>
+  
+        {/* Rest of your code here */}
+  
+      </main>
   )
 }
