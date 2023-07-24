7cfda64b95f53aec1914ae6e95f0108da1a146fb
Benedict Lee <benedictlee98@hotmail.com>
2023-07-24 15:45:27 -0500

    "Git commit test for personal use. Please do not punish me senpai!"

--- src/app/api/call-old/route.ts
+++ src/app/api/call-old/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config({ path: `.env.local` });
 
 interface Belva_ret {
