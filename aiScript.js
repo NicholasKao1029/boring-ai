2f0a23bcbcacda002bdef5efdf648ad209fa9d6f
Nicholas Koben Kao <kobenkao@gmail.com>
2023-07-24 20:41:54 -0700

    blah

--- aiScript.js
+++ aiScript.js
@@ -1,16 +1,19 @@
-import {config} from "dotev"
-config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-import {Configuration, OpenAIApi} from "openai"
+const { Configuration, OpenAIApi } = require("openai");
 
-const openai = new OpenAIApi=(new Configuration( {
-    apiKey: process.env.API_KEY
-}))
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
 
-openai.createChatCompletion({
-    model:"gpt-3.5-turbo",
-    messages:[{role:"user", content: "Hello ChatGPT"}]
-})
-.then(res => {
-    console.log(res.data.choices)
-})
+openai
+  .createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello ChatGPT" }],
+  })
+  .then((res) => {
+    console.log(res.data.choices);
+  });
