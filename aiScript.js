c12536bf007b390c0f965ec9af926b70f854f3c4
Benedict Lee <benedictlee98@hotmail.com>
2023-07-25 10:27:01 -0500

    Working Chat GPT API.
    Can interact with ChatGPT through the console. Use "npm run aiScript"

--- aiScript.js
+++ aiScript.js
@@ -1,16 +1,25 @@
-import {config} from "dotev"
+import {config} from "dotenv"
 config()
-
+import readline from "readline"
 import {Configuration, OpenAIApi} from "openai"
 
-const openai = new OpenAIApi=(new Configuration( {
-    apiKey: process.env.API_KEY
-}))
+const openai = new OpenAIApi( new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
 
-openai.createChatCompletion({
-    model:"gpt-3.5-turbo",
-    messages:[{role:"user", content: "Hello ChatGPT"}]
+const userInterface = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
 })
-.then(res => {
-    console.log(res.data.choices)
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+    const res = await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    })
+    console.log(res.data.choices[0].message.content);
+    userInterface.prompt()
 })
